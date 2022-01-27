f1069bd05ff0f43f05913e17c3daee3b89b685d0
isaiaspires <95889740+isaiaspires@users.noreply.github.com>
2022-01-27 14:44:55 -0300

    teste

---
+++ bin/aula_git.dart
@@ -0,0 +1,3 @@
+void main() {
+  print("Meu repositório no Git");
+}
---
+++ test/aula_git_test.dart
@@ -0,0 +1,8 @@
+import 'package:aula_git/aula_git.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
